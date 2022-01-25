c2e717d06e0222a47ef00c0034027abba39824b6
InfiSupaSta <88926276+InfiSupaSta@users.noreply.github.com>
2022-01-25 20:49:07 +0700

    Update settings.py

--- FirstDjangoProject/settings.py
+++ FirstDjangoProject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'FirstDjangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'smthfortest/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
