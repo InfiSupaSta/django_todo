c6f51fdabd816d098f00a835f9003e12b32be747
InfSuperStar <infiwow777@yandex.ru>
2022-01-25 21:57:17 +0700

    templates dir added

--- smthfortest/views.py
+++ smthfortest/views.py
@@ -17,6 +17,7 @@ from .utils import menu
 class MainPage(DataMixin, TemplateView):
     template_name = 'smthfortest\\base_template.html'
 
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         datamixin_context = self.get_user_context(title='О проекте')
