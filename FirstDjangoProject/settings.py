08e5a84de151543bc4f67d3d189a85a94790e813
InfiSupaSta <88926276+InfiSupaSta@users.noreply.github.com>
2022-01-25 21:13:03 +0700

    Update settings.py

--- FirstDjangoProject/settings.py
+++ FirstDjangoProject/settings.py
@@ -25,7 +25,7 @@ except (NameError, AttributeError):
     SECRET_KEY = 'Something secret here! Tss...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
